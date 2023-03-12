21f0ceb20c58edff0afd78775395d489cf9615e2
Max Navasyolau <madnovos@mail.ru>
2023-03-12 13:06:13 +0300

    Sunday end study

--- Code_Python/draft.py
+++ Code_Python/draft.py
@@ -1,14 +1,3 @@
-word = "слово"
-
-result = []
-
-for i  in range(len(word)):
-    letter = word[i].lower() if i % 2 != 0 else word[i].upper()
-    result.append(letter)
-   
-
-result = ''.join(result)
-print(result)            
-
-
-
+print("Начало")
+raise Exception('Что то пошло не так')
+print('Конец')
