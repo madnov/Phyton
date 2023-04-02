20637b931262f16b96a35f3d0facabdaa95447b4
Max Navasyolau <madnovos@mail.ru>
2023-04-02 18:37:21 +0300

    Sunday training

--- Code_Python/draft.py
+++ Code_Python/draft.py
@@ -1,17 +1,7 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def main():
-    return "<h1>Hello, World</h1><br><a href='/index'>перейти на вторую страницу</a>"
-
-
-@app.route('/index/<x>/<y>')
-def index(x, y):
-    return f"Результат {int(x) + int(y)}"
-
-
-if __name__ == '__main__':
-    app.run()
+vowels = 'aeouiy'
+string = "This website is for losers LOL!"
+for x in vowels:
+    if x.lower() in string:
+        string = string.replace(x.lower(), '')
+        
+print(string)        
